282f416a86d180364a39e8d84c6a2ebc4755d828
praveen-datascience <125889920+praveen-datascience@users.noreply.github.com>
2023-05-10 16:50:48 +0100

    Update index.py
    Required libraries are imported
    function is added to read excel file

--- index.py
+++ index.py
@@ -5,3 +5,22 @@ Created on Wed May 10 16:34:04 2023
 @author: Anthonimuthu Praveenkumar
 """
 
+#required libraries
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+from sklearn.decomposition import PCA
+from IPython.display import clear_output
+from sklearn.cluster import KMeans
+
+def read_my_excel(filename):
+    """ This function is used to read excel file and return dataframe """
+    excel_result = pd.read_excel(filename)
+    return excel_result
+
+#read csv file
+df_coemission = read_my_excel("co2_emission_updated.xls")
+
+#check initial dataframe
+print(df_coemission.head())
