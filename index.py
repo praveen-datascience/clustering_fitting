993d626590f6ef562bb829f6051ad12d6267f73b
praveen-datascience <125889920+praveen-datascience@users.noreply.github.com>
2023-05-10 17:37:52 +0100

    Update index.py
    K-means clustering algorithm
    First 2 steps are added
    1.Scale the data
    2.Initialize centroids

--- index.py
+++ index.py
@@ -22,12 +22,33 @@ def read_my_excel(filename):
 #read csv file
 df_coemission = read_my_excel("co2_emission_updated.xls")
 #check initial dataframe
-print(df_coemission.head())
+#print(df_coemission.head())
 
 #selecting required columns
-selected_features = ["Country Name", "Country Code", "1999", "2004", "2009", "2014", "2019"]
+selected_features = ["1999", "2004", "2009", "2014", "2019"]
 #empty and null values are cleaned
 df_coemission = df_coemission.dropna(subset=selected_features)
 #copy cleaned required data
 data = df_coemission[selected_features].copy()
-print(data.head())
+#print(data.head())
+
+#kmeans-steps
+#step1 : scale the data so that no one column will not dominate other column
+#here i used the range from 1 to 10
+data = ((data - data.min()) / (data.max() - data.min())) * 10 + 1
+data.describe()
+
+#step2 : Initialize random centroids
+def choose_random_centroids(data, k):
+    """ This function is used to select random centre values for each column """
+    centroids = []
+    for i in range(k):
+        #apply method used to iterate through each column and sample method select random value in each column
+        centroid = data.apply(lambda x: float(x.sample()))
+        centroids.append(centroid)
+    return pd.concat(centroids, axis=1)
+
+#function calling
+centroids = choose_random_centroids(data, 5)
+#check centre points
+print(centroids)
